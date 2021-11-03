8ec183e7a84fe46417a5c076382326f58137b676
rglover <me@ryanglover.net>
2021-11-02 20:40:30 -0500

    update release scripts to avoid version collisions add automatic git bump to release scripts add developmentVersion field to all packages for test releases on localhost

--- ui/release.js
+++ ui/release.js
@@ -1,11 +1,17 @@
 import { execSync } from "child_process";
 import semver from "semver";
 import { createRequire } from "module";
+import fs from 'fs';
 
 const require = createRequire(import.meta.url);
 const packageJSON = require("./package.json");
 
-const version = semver.inc(packageJSON.version, "prerelease", "beta");
+const originalVersion = `${packageJSON.version}`;
+const version = semver.inc(
+  process.env.NODE_ENV === 'development' ? packageJSON.developmentVersion : packageJSON.version,
+  "prerelease",
+  "beta"
+);
 const force = process.env.NODE_ENV === "development" ? "--force" : "";
 const registry =
   process.env.NODE_ENV === "development"
@@ -15,3 +21,14 @@ const registry =
 execSync(
   `npm version ${version} --allow-same-version ${registry} && npm publish --access public ${force} ${registry}`
 );
+
+if (process.env.NODE_ENV === 'production') {
+  execSync(`git add . && git commit -m "release @joystick.js/ui@${version}"`);
+}
+
+if (process.env.NODE_ENV === 'development') {
+  packageJSON.version = originalVersion;
+  packageJSON.developmentVersion = version;
+  console.log({ originalVersion, version });
+  fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, 2))
+}
