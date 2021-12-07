33b671604b4407da736adeaabc1ed643de0f4317
rglover <me@ryanglover.net>
2021-12-07 10:15:08 -0600

    add test helpers back in after accidental delete

---
+++ node/tests/lib/databases/mongodb/reset.js
@@ -0,0 +1,19 @@
+export default async (db = null) => {
+  try {
+    const collections = db && (await db.listCollections().toArray());
+
+    for (let i = 0; i < collections.length; i += 1) {
+      const collectionFromList = collections[i];
+      const collectionName = collectionFromList && collectionFromList.name;
+      const collection = collectionName && db.collection(collectionName);
+  
+      if (collection) {
+        await collection.deleteMany();
+      }
+    }
+  
+    return true;
+  } catch (exception) {
+    console.warn(exception);
+  }
+};
