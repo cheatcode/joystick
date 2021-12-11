e70ab9fb6dc87301f5216974338e49ec6fd921c6
rglover <me@ryanglover.net>
2021-12-11 16:20:35 -0600

    add __filename and __dirname polyfills update readme to explain usage of __filename and __dirname polyfills

--- node/dist/index.js
+++ node/dist/index.js
@@ -1,5 +1,6 @@
 import app from "./app/index.js";
 import api from "./api/index.js";
+import nodeUrlPolyfills from "./lib/nodeUrlPolyfills.js";
 import generateId from "./lib/generateId.js";
 import loadSettings from "./settings/load";
 import sendEmail from "./email/send";
@@ -8,6 +9,8 @@ const set = api.set;
 const email = {
   send: sendEmail
 };
+const __filename = nodeUrlPolyfills.__filename;
+const __dirname = nodeUrlPolyfills.__dirname;
 const settings = loadSettings();
 global.joystick = {
   id: generateId,
@@ -18,6 +21,8 @@ var src_default = {
   settings
 };
 export {
+  __dirname,
+  __filename,
   src_default as default,
   email,
   get,
--- node/src/index.js
+++ node/src/index.js
@@ -1,14 +1,18 @@
 import app from "./app/index.js";
 import api from "./api/index.js";
+import nodeUrlPolyfills from "./lib/nodeUrlPolyfills.js";
 import generateId from "./lib/generateId.js";
 import loadSettings from "./settings/load";
 import sendEmail from "./email/send";
+
 
 export const get = api.get;
 export const set = api.set;
 export const email = {
   send: sendEmail,
 };
+export const __filename = nodeUrlPolyfills.__filename;
+export const __dirname = nodeUrlPolyfills.__dirname;
 
 const settings = loadSettings();
 
