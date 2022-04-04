e288c820b68679466c38697a4d20855b6079ef48
rglover <me@ryanglover.net>
2022-04-04 17:28:29 -0500

    release @joystick.js/node@1.0.0-beta.40

--- node/dist/lib/parseDatabasesFromEnvironment.js
+++ node/dist/lib/parseDatabasesFromEnvironment.js
@@ -1,6 +1,5 @@
 var parseDatabasesFromEnvironment_default = (databases = "") => {
   if (databases && typeof databases === "string") {
-    console.log("DATABASES", databases);
     return JSON.parse(databases);
   }
   return {};
--- node/src/lib/parseDatabasesFromEnvironment.js
+++ node/src/lib/parseDatabasesFromEnvironment.js
@@ -1,6 +1,5 @@
 export default (databases = "") => {
   if (databases && typeof databases === "string") {
-    console.log('DATABASES', databases);
     return JSON.parse(databases);
   }
 
