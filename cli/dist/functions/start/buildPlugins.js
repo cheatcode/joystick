63206748a67b1932c719c9eb6cdfef220e88ca30
rglover <me@ryanglover.net>
2022-04-19 21:03:57 -0500

    release @joystick.js/cli@1.0.0-beta.34

--- cli/dist/functions/start/buildPlugins.js
+++ cli/dist/functions/start/buildPlugins.js
@@ -81,106 +81,6 @@ var buildPlugins_default = {
       });
     }
   },
-  bootstrapLayoutComponent: {
-    name: "bootstrapLayoutComponent",
-    setup(build) {
-      build.onLoad({ filter: /\.js$/ }, (args) => {
-        try {
-          const shouldBootstrap = [getPlatformSafePath("ui/layouts")].some((bootstrapTarget) => {
-            return args.path.includes(bootstrapTarget);
-          });
-          if (shouldBootstrap) {
-            const code = fs.readFileSync(getPlatformSafePath(args.path), "utf-8");
-            const joystickUIMatches = code.match(JOYSTICK_UI_REGEX) || [];
-            const joystickUIMatch = joystickUIMatches && joystickUIMatches[0];
-            const exportDefaultMatches = code.match(EXPORT_DEFAULT_REGEX) || [];
-            const exportDefaultMatch = exportDefaultMatches && exportDefaultMatches[0];
-            if (joystickUIMatch && !exportDefaultMatch) {
-              console.log(" ");
-              console.warn(chalk.yellowBright(`All Joystick components in the ui/layouts directory must have an export default statement (e.g., export default MyLayout). Please check the file at ${args.path}.`));
-              console.log(" ");
-              return;
-            }
-            const matchParts = exportDefaultMatch && exportDefaultMatch.split(" ") || [];
-            const componentName = matchParts.pop();
-            if (componentName) {
-              const code2 = fs.readFileSync(getPlatformSafePath(args.path), "utf-8");
-              return {
-                contents: code2.replace(`${exportDefaultMatch};`, `if (
-                    typeof window !== 'undefined' &&
-                    window.__joystick_ssr__ === true &&
-                    window.__joystick_layout_page__ &&
-                    ui &&
-                    ui.mount
-                  ) {
-                    (async () => {
-                      const layoutComponentFile = await import(window.__joystick_layout__);
-                      const pageComponentFile = await import(window.window.__joystick_layout_page_url__);
-                      const layout = layoutComponentFile.default;
-                      const page = pageComponentFile.default;
-                      ui.mount(layout, Object.assign({ ...window.__joystick_ssr_props__ }, { page }), document.getElementById('app'));
-                    })();
-                  }
-                
-                export default ${componentName};
-                  `),
-                loader: "js"
-              };
-            }
-          }
-        } catch (exception) {
-          console.warn(exception);
-        }
-      });
-    }
-  },
-  bootstrapPageComponent: {
-    name: "bootstrapPageComponent",
-    setup(build) {
-      build.onLoad({ filter: /\.js$/ }, (args) => {
-        try {
-          const shouldBootstrap = [getPlatformSafePath("ui/pages")].some((bootstrapTarget) => {
-            return args.path.includes(bootstrapTarget);
-          });
-          if (shouldBootstrap) {
-            const code = fs.readFileSync(getPlatformSafePath(args.path), "utf-8");
-            const joystickUIMatches = code.match(JOYSTICK_UI_REGEX) || [];
-            const joystickUIMatch = joystickUIMatches && joystickUIMatches[0];
-            const exportDefaultMatches = code.match(EXPORT_DEFAULT_REGEX) || [];
-            const exportDefaultMatch = exportDefaultMatches && exportDefaultMatches[0];
-            if (joystickUIMatch && !exportDefaultMatch) {
-              console.log(" ");
-              console.warn(chalk.yellowBright(`All Joystick components in the ui/pages directory must have an export default statement (e.g., export default MyPage). Please check the file at ${args.path}.`));
-              console.log(" ");
-              return;
-            }
-            const matchParts = exportDefaultMatch && exportDefaultMatch.split(" ") || [];
-            const componentName = matchParts.pop();
-            if (componentName) {
-              const code2 = fs.readFileSync(getPlatformSafePath(args.path), "utf-8");
-              return {
-                contents: code2.replace(`${exportDefaultMatch};`, `if (
-                    typeof window !== 'undefined' &&
-                    window.__joystick_ssr__ === true &&
-                    !window.__joystick_layout_page__ &&
-                    ui &&
-                    ui.mount
-                  ) {
-                    ui.mount(${componentName}, window.__joystick_ssr_props__ || {}, document.getElementById('app'));
-                  }
-                  
-                  export default ${componentName};
-                  `),
-                loader: "js"
-              };
-            }
-          }
-        } catch (exception) {
-          console.warn(exception);
-        }
-      });
-    }
-  },
   generateFileDependencyMap: {
     name: "generateFileDependencyMap",
     setup(build) {
@@ -200,26 +100,6 @@ var buildPlugins_default = {
           }
         } catch (exception) {
           console.warn(exception);
-        }
-      });
-    }
-  },
-  ssrId: {
-    name: "ssrId",
-    setup(build) {
-      build.onLoad({ filter: /\.js$/ }, (args) => {
-        const shouldSetId = [getPlatformSafePath("ui/")].some((bootstrapTarget) => {
-          return args.path.includes(bootstrapTarget);
-        });
-        if (shouldSetId) {
-          const code = fs.readFileSync(getPlatformSafePath(args.path), "utf-8");
-          const ssrId = generateId();
-          const contents = code.replace("ui.component({", `ui.component({
-  _ssrId: '${ssrId}',`);
-          return {
-            contents,
-            loader: "js"
-          };
         }
       });
     }
