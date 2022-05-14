07a61389a1408210aaaeb576c688560851eb5379
rglover <me@ryanglover.net>
2022-05-14 18:06:13 -0500

    release @joystick.js/node@1.0.0-beta.63

--- node/src/app/index.js
+++ node/src/app/index.js
@@ -108,8 +108,9 @@ export class App {
   initDeploy() {
     if (process.env.NODE_ENV === 'production') {
       this.express.app.get('/api/_deploy/logs', async (req, res) => {
-        const deployToken = fs.readFileSync('/root/token.txt', 'utf-8');
-        if (req?.headers['x-instance-token'] === deployToken?.replace('\n', '')) {
+        const instanceToken = fs.readFileSync('/root/token.txt', 'utf-8');
+        console.log({ thing: 'logs', instanceToken, header: req?.headers['x-instance-token'] });
+        if (req?.headers['x-instance-token'] === instanceToken?.replace('\n', '')) {
           const logs = await getLogs(req?.query);
           return res.status(200).send(logs);
         }
@@ -118,8 +119,9 @@ export class App {
       });
   
       this.express.app.get('/api/_deploy/metrics', async (req, res) => {
-        const deployToken = fs.readFileSync('/root/token.txt', 'utf-8');
-        if (req?.headers['x-instance-token'] === deployToken?.replace('\n', '')) {
+        const instanceToken = fs.readFileSync('/root/token.txt', 'utf-8');
+        console.log({ thing: 'metrics', instanceToken, header: req?.headers['x-instance-token'] });
+        if (req?.headers['x-instance-token'] === instanceToken?.replace('\n', '')) {
           const metrics = await getMetrics();
           return res.status(200).send(metrics);
         }
