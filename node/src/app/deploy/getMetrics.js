14f6a15ab9cb71a0c1ace6265087ce8ae4f56ec3
rglover <me@ryanglover.net>
2022-05-15 17:19:10 -0500

    release @joystick.js/node@1.0.0-beta.67

--- node/src/app/deploy/getMetrics.js
+++ node/src/app/deploy/getMetrics.js
@@ -6,6 +6,6 @@ export default () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   // NOTE: Use __dirname so that we get the path to this file from within the @joystick.js/node package and
-  // note the default application root.
-  return execSync(`chmod +x ${__dirname}/snapshot-metrics && .${__dirname}/snapshot-metrics`);
+  // note the default application root. Replace /root in second command as it's already being run from the /root directory.
+  return execSync(`chmod +x ${__dirname}/snapshot-metrics && .${__dirname?.replace('/root', '.')}/snapshot-metrics`);
 };
