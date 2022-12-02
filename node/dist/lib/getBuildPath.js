805829e1ab93fbe8c558bda957d78c2aefef479e
rglover <me@ryanglover.net>
2022-12-02 13:26:54 -0600

    release @joystick.js/node@1.0.0-beta.109

--- node/dist/lib/getBuildPath.js
+++ node/dist/lib/getBuildPath.js
@@ -1,5 +1,6 @@
+import fs from "fs";
 var getBuildPath_default = () => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "development" || fs.existsSync(".joystick/build")) {
     return ".joystick/build/";
   }
   return "";
--- node/src/lib/getBuildPath.js
+++ node/src/lib/getBuildPath.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+
 export default () => {
-  if (process.env.NODE_ENV === 'development') {
+  // NOTE: The fs.existsSync() check here is determining if we're running the app in a non-development
+  // environment locally on our computer. If we do something like joystick start -e production locally,
+  // we'll still have a .joystick/build folder (whereas in an actual production build, we do not). This
+  // ensures that we don't break the render when looking for the built copy of a page/layout. Without this,
+  // we accidentally pull back the root directory and attempt to render an unbuilt copy of the page/layout.
+  if (process.env.NODE_ENV === 'development' || fs.existsSync('.joystick/build')) {
     return '.joystick/build/';
   }
 
