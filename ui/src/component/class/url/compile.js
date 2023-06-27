0c357cc4d9f1615cc2d11567657348bad426a094
rglover <me@ryanglover.net>
2023-06-27 13:55:30 -0500

    release @joystick.js/ui@1.0.0-beta.77

--- ui/src/component/class/url/compile.js
+++ ui/src/component/class/url/compile.js
@@ -5,15 +5,35 @@ export default (url = {}) => {
   try {
     return {
       ...url,
-      isActive: (path = '') => {
-        if (isString(path) && url?.route !== '*') {
-          return path === (typeof location !== 'undefined' ? location.pathname : url.path);
+      query: {
+        ...(url?.query || {}),
+        set: (parameterName = "", parameterValue = "") => {
+          if (typeof window !== "undefined") {
+            const url = new URL(window.location);
+            url.searchParams.append(parameterName, parameterValue);
+            window.history.pushState({}, "", url);
+          }
+        },
+        unset: (parameterName = "") => {
+          if (typeof window !== "undefined") {
+            const url = new URL(window.location);
+            url.searchParams.delete(parameterName);
+            window.history.pushState({}, "", url);
+          }
+        },
+      },
+      isActive: (path = "") => {
+        if (isString(path) && url?.route !== "*") {
+          return (
+            path ===
+            (typeof location !== "undefined" ? location.pathname : url.path)
+          );
         }
 
         return false;
       },
     };
   } catch (exception) {
-    throwFrameworkError('component.url.compile', exception);
+    throwFrameworkError("component.url.compile", exception);
   }
-}
+};
