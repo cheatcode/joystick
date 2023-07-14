a918bd4473925b1bf38caf4c34261d02751d91f4
rglover <me@ryanglover.net>
2023-07-14 13:10:29 -0500

    release @joystick.js/node@1.0.0-beta.155

--- node/dist/app/middleware/insecure.js
+++ node/dist/app/middleware/insecure.js
@@ -1,5 +1,7 @@
 var insecure_default = (req, res, next) => {
-  if (process.env.NODE_ENV != "development" && !req.secure) {
+  const forwardedProtocol = req.get("x-forwarded-proto");
+  const isForwardedFromHTTPS = forwardedProtocol && forwardedProtocol === "https";
+  if (process.env.NODE_ENV != "development" && !req.secure && !isForwardedFromHTTPS) {
     return res.redirect("https://" + req.headers.host + req.url);
   }
   next();
--- node/src/app/middleware/insecure.js
+++ node/src/app/middleware/insecure.js
@@ -1,5 +1,10 @@
 export default (req, res, next) => {
-  if (process.env.NODE_ENV != 'development' && !req.secure) {
+  // NOTE: Check the x-forwarded-proto call to see if this request was made
+  // via HTTPS where the SSL was terminated at a load balancer.
+  const forwardedProtocol = req.get("x-forwarded-proto");
+  const isForwardedFromHTTPS = forwardedProtocol && forwardedProtocol === 'https';
+  
+  if (process.env.NODE_ENV != 'development' && !req.secure && !isForwardedFromHTTPS) {
     return res.redirect("https://" + req.headers.host + req.url);
   }
 
