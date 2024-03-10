9e258e7a15f092f35d6151d708b573b5504fb99a
rglover <me@ryanglover.net>
2024-03-10 07:57:48 -0500

    remove unnecessary comment in build command refactor custom copy paths for build command into function

--- cli/src/lib/build/index.js
+++ cli/src/lib/build/index.js
@@ -16,6 +16,31 @@ import encrypt_buffer from '../../lib/encrypt_buffer.js';
 const { mkdir, copyFile, readFile, writeFile, readdir } = fs.promises;
 const exec = util.promisify(child_process.exec);
 
+const get_custom_copy_paths = async (copy_paths_from_settings = []) => {
+  const custom_copy_paths = [];
+
+  for (let i = 0; i < copy_paths_from_settings?.length; i += 1) {
+    const custom_copy_path = copy_paths_from_settings[i];
+
+    if (fs.existsSync(custom_copy_path)) {
+      const stat = fs.lstatSync(custom_copy_path);
+
+      if (stat.isDirectory()) {
+        const paths = await readdir(custom_copy_path, { recursive: true });
+        custom_copy_paths.push(
+          ...(paths || [])?.map((path) => {
+            return `${custom_copy_path}/${path}`;
+          })
+        );
+      } else {
+        custom_copy_paths.push(custom_copy_path);
+      }
+    }
+  }
+
+  return custom_copy_paths;
+};
+
 const get_files_to_build_with_operation_and_platform = (files = []) => {
   return files?.map((file) => {
     return {
@@ -36,28 +61,7 @@ const build = async (options = {}) => {
 
   const settings = await load_settings(environment);
   const excluded_paths = settings?.config?.build?.excluded_paths || settings?.config?.build?.excludedPaths;
-  const custom_copy_paths = [];
-
-  for (let i = 0; i < settings?.config?.build?.copy_paths?.length; i += 1) {
-    const custom_copy_path = settings?.config?.build?.copy_paths[i];
-
-    if (fs.existsSync(custom_copy_path)) {
-      const stat = fs.lstatSync(custom_copy_path);
-
-      if (stat.isDirectory()) {
-        const paths = await readdir(custom_copy_path, { recursive: true });
-        custom_copy_paths.push(
-          ...(paths || [])?.map((path) => {
-            return `${custom_copy_path}/${path}`;
-          })
-        );
-      } else {
-        custom_copy_paths.push(custom_copy_path);
-      }
-    }
-  }
-
-  console.log(custom_copy_paths);
+  const custom_copy_paths = get_custom_copy_paths(settings?.config?.build?.copy_paths);
 
   const files_for_build = await get_files_to_build(excluded_paths, custom_copy_paths);
   const output_path = build_type === 'tar' ? '.build/.tar' : '.build';
