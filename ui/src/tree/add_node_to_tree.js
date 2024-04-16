8ccecac3b6fbc7e70d2267596ff80b389087f957
rglover <me@ryanglover.net>
2024-04-16 15:07:26 -0500

    wip debugging node capture

--- ui/src/tree/add_node_to_tree.js
+++ ui/src/tree/add_node_to_tree.js
@@ -1,13 +1,17 @@
 const add_node_to_tree = (node = {}, ssr_tree = null) => {
   const tree_on_window = typeof window !== 'undefined' ? window.joystick._internal.tree : [];
 
+  (ssr_tree || tree_on_window).push(node);
+
   console.log(JSON.stringify({
+    ssr_tree: !!ssr_tree,
     has_tree: typeof window !== 'undefined' && !!window.joystick._internal.tree,
     node_id: node?.id,
     node_css: node?.css,
+    can_find_in_tree: typeof window !== 'undefined' && window.joystick._internal.tree?.some((node_in_tree) => {
+      return node_in_tree.id === node.id;
+    }),
   }));
-
-  (ssr_tree || tree_on_window).push(node);
 };
 
 export default add_node_to_tree;
