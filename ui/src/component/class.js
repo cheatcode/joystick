3f43d1478a8231aab76f342bd56b2ce6de5ede1a
rglover <me@ryanglover.net>
2024-05-01 17:51:00 -0500

    wip debugging node capture

--- ui/src/component/class.js
+++ ui/src/component/class.js
@@ -259,12 +259,12 @@ class Component {
 			options.after_refetch_data_rerender();
 		}
 
-		// NOTE: Clean up the linked list by removing any nodes matching an ID
-		// in existing_children as we know they no longer exist.
-		// clean_up_tree();
-
 		// NOTE: Do after clean up so we don't reattach styles for old nodes.
 		run_tree_job('css');
+
+		// NOTE: Clean up the linked list by removing any nodes matching an ID
+		// in existing_children as we know they no longer exist.
+		clean_up_tree();
 	}
 
 	sanitize_html(html = '') {
--- ui/src/tree/clean_up.js
+++ ui/src/tree/clean_up.js
@@ -1,11 +1,14 @@
+import debounce from "../lib/debounce.js";
 import get_child_instance_ids from "./get_child_instance_ids.js";
 
 const clean_up = () => {
-	// NOTE: If we can't find the instance_id in the DOM, we know that the instance
-	// was nuked during re-render and can safely be removed from the tree.
-	joystick._internal.tree = joystick?._internal?.tree?.filter((node) => {
-		return !!document.querySelector(`[js-i="${node?.instance_id}"]`);
-	});
+	debounce(() => {
+		// NOTE: If we can't find the instance_id in the DOM, we know that the instance
+		// was nuked during re-render and can safely be removed from the tree.
+		joystick._internal.tree = joystick?._internal?.tree?.filter((node) => {
+			return !!document.querySelector(`[js-i="${node?.instance_id}"]`);
+		});
+	}, 500);
 };
 
 export default clean_up;
