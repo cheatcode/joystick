48ab1aa2b626c94a2935ff3575998d54a16077fb
rglover <me@ryanglover.net>
2024-05-09 15:19:54 -0500

    swap get with find and find_one in cache

--- node/src/cache/index.js
+++ node/src/cache/index.js
@@ -6,10 +6,15 @@ const cache = (cache_name = '') => {
         cache_item,
       ];
     },
-    get: ([key = '', value = '']) => {
-      return process.caches[cache_name]?.find((cache_item) => {
-        return cache_item[key] === value;
-      });
+    find: (query_array = null) => {
+      return query_array ? process.caches[cache_name]?.filter((cache_item) => {
+        return cache_item[query_array[0]] === query_array[1];
+      }) : process.caches[cache_name];
+    },
+    find_one: (query_array = null) => {
+      return query_array ? process.caches[cache_name]?.find((cache_item) => {
+        return cache_item[query_array[0]] === query_array[1];
+      }) : null;
     },
     set: (cache_array = []) => {
       process.caches[cache_name] = cache_array;
