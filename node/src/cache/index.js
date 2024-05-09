de92450f7b002313c50d1c80ccdbf619038d8a2c
rglover <me@ryanglover.net>
2024-05-09 18:27:46 -0500

    wip debugging cache update order

--- node/src/cache/index.js
+++ node/src/cache/index.js
@@ -49,33 +49,39 @@ const cache = (cache_name = '') => {
       }
 
       process.caches._queues[cache_name].add(() => {
-        // NOTE: Find the item to update.
-        let item_to_update_in_cache = process.caches[cache_name]?.find((cache_item) => {
-          return cache_item[key_to_match] = value_to_match;
-        });
-
-        if (item_to_update_in_cache) {
-          // NOTE: If we find a match, remove the original from the cache.
-          process.caches[cache_name] = (process.caches[cache_name] || [])?.filter((cache_item = {}) => {
-            if (!cache_item[key_to_match]) {
-              return true;
-            }
-
-            return cache_item[key_to_match] !== value_to_match;
+        return new Promise((resolve) => {
+          // NOTE: Find the item to update.
+          let item_to_update_in_cache = process.caches[cache_name]?.find((cache_item) => {
+            return cache_item[key_to_match] = value_to_match;
           });
 
-          // NOTE: Make the patches to the matched item.
-          item_to_update_in_cache = {
-            ...(item_to_update_in_cache || {}),
-            ...(replacement_item || {}),
-          };
+          if (item_to_update_in_cache) {
+            // NOTE: If we find a match, remove the original from the cache.
+            process.caches[cache_name] = (process.caches[cache_name] || [])?.filter((cache_item = {}) => {
+              if (!cache_item[key_to_match]) {
+                return true;
+              }
 
-          // NOTE: Add the patched item back to the cache.
-          process.caches[cache_name] = [
-            ...(process.caches[cache_name] || []),
-            item_to_update_in_cache,
-          ];
-        }
+              return cache_item[key_to_match] !== value_to_match;
+            });
+
+            // NOTE: Make the patches to the matched item.
+            item_to_update_in_cache = {
+              ...(item_to_update_in_cache || {}),
+              ...(replacement_item || {}),
+            };
+
+            // NOTE: Add the patched item back to the cache.
+            process.caches[cache_name] = [
+              ...(process.caches[cache_name] || []),
+              item_to_update_in_cache,
+            ];
+          }
+
+          setTimeout(() => {
+            resolve();
+          }, 100);
+        });
       });
     },
   };
