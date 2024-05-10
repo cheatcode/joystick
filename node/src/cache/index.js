dd290dd94165fc75dfc790c9809bdb2cff8182db
rglover <me@ryanglover.net>
2024-05-09 19:03:03 -0500

    experiment with direct index edit on cache

--- node/src/cache/index.js
+++ node/src/cache/index.js
@@ -20,33 +20,44 @@ const cache = (cache_name = '') => {
       process.caches[cache_name] = cache_array;
     },
     update: ([key_to_match = '', value_to_match = ''], replacement_item = {}) => {
-      // NOTE: Find the item to update.
-      let item_to_update_in_cache = process.caches[cache_name]?.find((cache_item) => {
-        return cache_item[key_to_match] = value_to_match;
+      const index_to_update = process.caches[cache_name]?.findIndex((cache_item = {}) => {
+        return cache_item[key_to_match] === value_to_match;
       });
 
-      if (item_to_update_in_cache) {
-        // NOTE: If we find a match, remove the original from the cache.
-        process.caches[cache_name] = (process.caches[cache_name] || [])?.filter((cache_item = {}) => {
-          if (!cache_item[key_to_match]) {
-            return true;
-          }
-
-          return cache_item[key_to_match] !== value_to_match;
-        });
-
-        // NOTE: Make the patches to the matched item.
-        item_to_update_in_cache = {
-          ...(item_to_update_in_cache || {}),
-          ...(replacement_item || {}),
+      if (index_to_update) {
+        process.caches[cache_name][index_to_update] = {
+          ...(process.caches[cache_name][index_to_update] || {}),
+          ...replacement_item,
         };
-
-        // NOTE: Add the patched item back to the cache.
-        process.caches[cache_name] = [
-          ...(process.caches[cache_name] || []),
-          item_to_update_in_cache,
-        ];
       }
+
+      // NOTE: Find the item to update.
+      // let item_to_update_in_cache = process.caches[cache_name]?.find((cache_item) => {
+      //   return cache_item[key_to_match] = value_to_match;
+      // });
+
+      // if (item_to_update_in_cache) {
+      //   // NOTE: If we find a match, remove the original from the cache.
+      //   process.caches[cache_name] = (process.caches[cache_name] || [])?.filter((cache_item = {}) => {
+      //     if (!cache_item[key_to_match]) {
+      //       return true;
+      //     }
+
+      //     return cache_item[key_to_match] !== value_to_match;
+      //   });
+
+      //   // NOTE: Make the patches to the matched item.
+      //   item_to_update_in_cache = {
+      //     ...(item_to_update_in_cache || {}),
+      //     ...(replacement_item || {}),
+      //   };
+
+      //   // NOTE: Add the patched item back to the cache.
+      //   process.caches[cache_name] = [
+      //     ...(process.caches[cache_name] || []),
+      //     item_to_update_in_cache,
+      //   ];
+      // }
     },
   };
 };
