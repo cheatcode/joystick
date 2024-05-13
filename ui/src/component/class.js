49d435ffa552bce1c61172d1bca12b526bc29622
rglover <me@ryanglover.net>
2024-05-13 12:16:07 -0500

    remove unnecessary log

--- ui/src/component/class.js
+++ ui/src/component/class.js
@@ -168,7 +168,6 @@ class Component {
 	render_to_html(new_children = {}, existing_children = {}, ssr_tree = null, linkedom_document = {}) {
 		const render_methods = this.compile_render_methods(new_children, existing_children, ssr_tree);
 		const html = this.options.render({ ...(this || {}), ...render_methods });
-		console.log('render_to_html', html);
 		const clean_html = this.cleanup_html(html, linkedom_document);
 		const sanitized_html = this.sanitize_html(clean_html);
 		const wrapped_html = this.wrap_html(sanitized_html);
