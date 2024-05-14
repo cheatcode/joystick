be2833704f6b795b1f0be75eac250ccded3c0988
rglover <me@ryanglover.net>
2024-05-14 13:51:55 -0500

    wip debugging vdom

--- ui/src/component/class.js
+++ ui/src/component/class.js
@@ -168,7 +168,6 @@ class Component {
 	render_to_html(new_children = {}, existing_children = {}, ssr_tree = null, linkedom_document = {}) {
 		const render_methods = this.compile_render_methods(new_children, existing_children, ssr_tree);
 		const html = this.options.render({ ...(this || {}), ...render_methods });
-		console.log(html);
 		const clean_html = this.cleanup_html(html, linkedom_document);
 		const sanitized_html = this.sanitize_html(clean_html);
 		const wrapped_html = this.wrap_html(sanitized_html);
@@ -230,6 +229,12 @@ class Component {
 		const new_html = this.replace_when_tags(component_html);
 		const new_dom = this.render_html_to_dom(new_html);
 		const new_virtual_dom = this.render_dom_to_virtual_dom(new_dom);
+		
+		console.log({
+			old: this.virtual_dom,
+			new: new_virtual_dom,
+		});
+
 		const dom_node_patches = diff_virtual_dom(this.virtual_dom, new_virtual_dom);
 
 		if (types.is_function(dom_node_patches)) {
