fa70bf3ee0d35f80fd6979ad21bcd88bb1773360
rglover <me@ryanglover.net>
2024-05-14 17:31:14 -0500

    adjust upload progress percentage

--- node/src/app/uploaders/local_upload_progress_middleware.js
+++ node/src/app/uploaders/local_upload_progress_middleware.js
@@ -8,7 +8,7 @@ const local_upload_progress_middleware = (providers = 1, req = {}, res = {}, nex
   req.on("data", (chunk) => {
     if (chunk.length > 0) {
       progress += chunk.length;
-      const percentage = float_to_decimal_place((progress / (file_size * providers)) * 100);
+      const percentage = float_to_decimal_place((progress / (file_size * providers)) * 100, 0);
       emit_websocket_event(
         `uploaders_${req?.headers["x-joystick-upload-id"]}`,
         "progress",
