407d7fc24234dd56c98d31546a82abf3531451b5
rglover <me@ryanglover.net>
2024-05-14 12:27:56 -0500

    wip debugging node patching

--- ui/src/component/virtual_dom/map_patch_functions_to_nodes.js
+++ ui/src/component/virtual_dom/map_patch_functions_to_nodes.js
@@ -1,12 +1,18 @@
 const map_patch_functions_to_nodes = (patch_functions = [], nodes = []) => {
   const map = [];
-  const longest_list_length = Math.max(patch_functions.length, nodes.length);
 
-  for (let i = 0; i < longest_list_length; i += 1) {
-    const patch_function = patch_functions[i];
+  for (let i = 0; i < nodes?.length; i += 1) {
     const node = nodes[i];
-    map.push([patch_function, node]);
+    map.push([patch_functions[i], node]);
   }
+  
+  // const longest_list_length = Math.max(patch_functions.length, nodes.length);
+
+  // for (let i = 0; i < longest_list_length; i += 1) {
+  //   const patch_function = patch_functions[i];
+  //   const node = nodes[i];
+  //   map.push([patch_function, node]);
+  // }
 
   return map;
 };
