b6596e61e8d474048b55d55660f7c073c2a0810a
rglover <me@ryanglover.net>
2024-05-14 23:29:44 -0500

    wip debugging data decode

--- ui/src/lib/decrypt_data_on_window.js
+++ ui/src/lib/decrypt_data_on_window.js
@@ -2,7 +2,8 @@ import decode_utf8_base64_string from "./decode_utf8_base64_string.js";
 
 const decrypt_data_on_window = () => {
   window.__joystick_data__ = Object.entries(window.__joystick_data__ || {})?.reduce((decoded, [key, value]) => {
-    decoded[key] = decode_utf8_base64_string(value);
+    const decoded_value = decode_utf8_base64_string(value);
+    decoded[key] = decoded_value ? JSON.parse(decoded_value) : {};
     return decoded;
   }, {});
 };
