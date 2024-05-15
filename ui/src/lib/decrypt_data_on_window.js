00e2e7148a8f6894e4989c254d291422987f0ff6
rglover <me@ryanglover.net>
2024-05-14 23:27:24 -0500

    wip debugging data decode

--- ui/src/lib/decrypt_data_on_window.js
+++ ui/src/lib/decrypt_data_on_window.js
@@ -1,3 +1,5 @@
+import decode_utf8_base64_string from "./decode_utf8_base64_string.js";
+
 const decrypt_data_on_window = () => {
   window.__joystick_data__ = Object.entries(window.__joystick_data__ || {})?.reduce((decoded, [key, value]) => {
     decoded[key] = decode_utf8_base64_string(value);
