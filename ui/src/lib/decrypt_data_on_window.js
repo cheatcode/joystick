18f4345c609a39db34b1361e7dcb2bee1f544501
rglover <me@ryanglover.net>
2024-05-14 23:26:07 -0500

    wip debugging data decode

--- ui/src/lib/decrypt_data_on_window.js
+++ ui/src/lib/decrypt_data_on_window.js
@@ -1,11 +1,8 @@
 const decrypt_data_on_window = () => {
-  const raw_data = JSON.parse(window.__joystick_data__ || '{}');
-  const decoded_data = Object.entries(raw_data)?.reduce((decoded, [key, value]) => {
+  window.__joystick_data__ = Object.entries(window.__joystick_data__ || {})?.reduce((decoded, [key, value]) => {
     decoded[key] = decode_utf8_base64_string(value);
     return decoded;
   }, {});
-
-  window.__joystick_data__ = decoded_data;
 };
 
 export default decrypt_data_on_window;
