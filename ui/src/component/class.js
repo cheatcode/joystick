95f794c7e20a911e4ce84c61bfa750d975d2d31f
rglover <me@ryanglover.net>
2024-05-14 21:27:55 -0500

    add timer to help debug rerender speed

--- ui/src/component/class.js
+++ ui/src/component/class.js
@@ -205,6 +205,7 @@ class Component {
 	}
 
 	rerender(options = {}) {
+		console.time('rerender');
 		track_function_call(`ui.${this?.options?.test?.name || generate_id()}.rerender`, [
 			options,
 		]);
@@ -266,6 +267,7 @@ class Component {
 		// NOTE: Clean up the linked list by removing any nodes matching an ID
 		// in existing_children as we know they no longer exist.
 		clean_up_tree();
+		console.timeEnd('rerender');
 	}
 
 	sanitize_html(html = '') {
