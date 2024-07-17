5c7d692ae48b61933da4aab9f170bde3a8aef4fb
rglover <me@ryanglover.net>
2024-07-17 18:15:52 -0500

    wip wiring up database binaries

--- cli/src/lib/development_server/databases/postgresql/index.js
+++ cli/src/lib/development_server/databases/postgresql/index.js
@@ -1,6 +1,7 @@
 import child_process from "child_process";
 import fs from "fs";
 import util from "util";
+import os from "os";
 import cli_log from "../../../cli_log.js";
 import command_exists from "../../../command_exists.js";
 import get_platform_safe_path from "../../../get_platform_safe_path.js";
@@ -42,31 +43,33 @@ const check_if_postgresql_control_exists = () => {
 };
 
 const start_postgresql = async (port = 2610) => {
-  const postgresql_exists = await check_if_postgresql_exists();
+  // const postgresql_exists = await check_if_postgresql_exists();
 
-  if (!postgresql_exists) {
-    warn_postgresql_is_missing();
-    process.exit(1);
-  }
+  // if (!postgresql_exists) {
+  //   warn_postgresql_is_missing();
+  //   process.exit(1);
+  // }
 
-  const postgresql_control_exists = await check_if_postgresql_control_exists();
+  // const postgresql_control_exists = await check_if_postgresql_control_exists();
 
-  if (!postgresql_control_exists) {
-    cli_log(
-      'PostgreSQL is installed on this computer, but pg_ctl (what Joystick uses to start and manage PostgreSQL) is not in your command line\'s PATH variable. Add pg_ctl to your command line\'s PATH, restart your command line, and try again.',
-      {
-        level: 'danger',
-        docs: 'https://cheatcode.co/docs/joystick/postgresql#path'
-      }
-    );
-  }
+  // if (!postgresql_control_exists) {
+  //   cli_log(
+  //     'PostgreSQL is installed on this computer, but pg_ctl (what Joystick uses to start and manage PostgreSQL) is not in your command line\'s PATH variable. Add pg_ctl to your command line\'s PATH, restart your command line, and try again.',
+  //     {
+  //       level: 'danger',
+  //       docs: 'https://cheatcode.co/docs/joystick/postgresql#path'
+  //     }
+  //   );
+  // }
 
   try {
+    const joystick_pg_ctl_path = `${os.homedir()}/.joystick/databases/postgresql/bin/bin/pg_ctl`;
+    const joystick_createdb_path = `${os.homedir()}/.joystick/databases/postgresql/bin/bin/createdb`;
     const data_directory_exists = await setup_data_directory(port);
 
-    if (!data_directory_exists && postgresql_control_exists) {
+    if (!data_directory_exists) {
       await exec(
-        `pg_ctl init -D .joystick/data/postgresql_${port} --options=--no-locale`
+        `${joystick_pg_ctl_path} init -D .joystick/data/postgresql_${port} --options=--no-locale`
       );
     }
 
@@ -74,11 +77,11 @@ const start_postgresql = async (port = 2610) => {
     const existing_process_id = parseInt(await get_process_id_from_port(postgresql_port), 10);
 
     if (existing_process_id) {
-      await exec(`pg_ctl kill KILL ${existing_process_id}`);
+      await exec(`${joystick_pg_ctl_path} kill KILL ${existing_process_id}`);
     }
 
     const database_process = child_process.spawn(
-      `pg_ctl`,
+      `${joystick_pg_ctl_path}`,
       [
         '-o',
         `"-p ${postgresql_port}"`,
@@ -103,7 +106,7 @@ const start_postgresql = async (port = 2610) => {
         if (stdout.includes('database system is ready to accept connections')) {
           const process_id = (await get_process_id_from_port(postgresql_port))?.replace('\n', '');
 
-          exec(`createdb -h 127.0.0.1 -p ${postgresql_port} app`).then((data) => {
+          exec(`${joystick_createdb_path} -h 127.0.0.1 -p ${postgresql_port} app`).then((data) => {
             resolve(parseInt(process_id, 10));
           }).catch(({ stderr: error }) => {
             // NOTE: PostgreSQL does not have a clean way to create database if it doesn't exist. Use this as a hack
