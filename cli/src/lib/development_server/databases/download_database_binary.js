f9d9894b085d5c3ebc36d5955512072be7d33281
rglover <me@ryanglover.net>
2024-07-17 21:57:28 -0500

    wip debugging database binaries

--- cli/src/lib/development_server/databases/download_database_binary.js
+++ cli/src/lib/development_server/databases/download_database_binary.js
@@ -178,7 +178,8 @@ const download_database_binary = async (database_name_lowercase, version_path = 
   await create_directories(base_directory, bin_directory);
 
   const download_url = await get_download_url(database_name_lowercase, version_path);
-  const file_name = path.basename(new URL(download_url).pathname);
+  const main_url = typeof download_url === 'object' ? download_url.main : download_url;
+  const file_name = path.basename(new URL(main_url).pathname);
   const file_path = path.join(base_directory, file_name);
 
   if (await check_if_file_exists(file_path)) {
@@ -186,7 +187,7 @@ const download_database_binary = async (database_name_lowercase, version_path = 
   }
 
   process.loader.print(`${database_name_map[database_name_lowercase]} not found. Downloading...`);
-  await download_file(download_url, file_path);
+  await download_file(main_url, file_path);
   process.loader.print(`Installing ${database_name_map[database_name_lowercase]}...`);
   await extract_and_build(database_name_lowercase, file_path, base_directory, bin_directory);
   await make_file_executable(bin_directory);
