fb8e77d8e806a0148fe96dff39c0864b9ad378ab
rglover <me@ryanglover.net>
2024-07-21 18:02:07 -0500

    wip refactoring database binaries

--- cli/src/lib/development_server/databases/postgresql/index.js
+++ cli/src/lib/development_server/databases/postgresql/index.js
@@ -113,7 +113,7 @@ const start_postgresql = async (port = 2610) => {
         'postgres',
         'bash',
         '-c',
-        `PGDATA="${pgdata_path}" ./postgres -p ${postgresql_port} > /dev/null 2>&1`,
+        `PGDATA="${get_platform_safe_path(`${process.cwd()}/.joystick/data/postgresql_${port}`)}" ./postgres -p ${postgresql_port} > /dev/null 2>&1`,
       ],
       // `sudo -u postgres bash ./postgres`,
       // [
