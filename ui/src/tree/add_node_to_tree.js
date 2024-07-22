f4560602de97c8934f5f0fe47880a1a206efdc25
rglover <me@ryanglover.net>
2024-07-22 17:58:56 -0500

    wip debugging child state

--- ui/src/tree/add_node_to_tree.js
+++ ui/src/tree/add_node_to_tree.js
@@ -3,8 +3,18 @@ import get_node_decimal_id from './get_node_decimal_id.js';
 const add_node_to_tree = (node = {}, ssr_tree = null) => {
   const tree_on_window = typeof window !== 'undefined' ? window.joystick._internal.tree : [];
   const tree = (ssr_tree || tree_on_window);
+
   node.decimal_id = get_node_decimal_id(tree?.length, node.parent);
   tree.push(node);
+
+  // NOTE: Return this as we can reference it later to find the state value in the
+  // component() render_method implementation.
+
+  if (node.DOMNode) {
+    node.DOMNode.setAttribute('data-decimal-id', node.decimal_id);
+  }
+  
+  return node.decimal_id;
 };
 
 export default add_node_to_tree;
