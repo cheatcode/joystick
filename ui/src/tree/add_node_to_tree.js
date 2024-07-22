6555389ee087a65b2b4f8bef4e4abbf0a42aedc0
rglover <me@ryanglover.net>
2024-07-22 17:52:39 -0500

    wip debugging child state

--- ui/src/tree/add_node_to_tree.js
+++ ui/src/tree/add_node_to_tree.js
@@ -1,7 +1,10 @@
+import get_node_decimal_id from './get_node_decimal_id.js';
+
 const add_node_to_tree = (node = {}, ssr_tree = null) => {
   const tree_on_window = typeof window !== 'undefined' ? window.joystick._internal.tree : [];
-
-  (ssr_tree || tree_on_window).push(node);
+  const tree = (ssr_tree || tree_on_window);
+  node.decimal_id = get_node_decimal_id(tree?.length, node.parent);
+  tree.push(node);
 };
 
 export default add_node_to_tree;
---
+++ ui/src/tree/get_node_decimal_id.js
@@ -0,0 +1,16 @@
+const get_node_decimal_id = (tree_length = 0, parent = null) => {
+  if (parent) {
+    const parent_decimal_id_parts = parent?.decimal_id?.split('.');
+    const last_part = parent_decimal_id_parts[parent_decimal_id_parts.length - 1];
+    const new_decimal =
+      parent_decimal_id_parts?.length > 1 ?
+        [...parent_decimal_id_parts, parseInt(last_part) + 1]?.join('.') :
+        [...parent_decimal_id_parts, 1]?.join('.');
+    
+    return new_decimal;
+  }
+
+  return tree_length + 1;
+};
+
+export default get_node_decimal_id;
