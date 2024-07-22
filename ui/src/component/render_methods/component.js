b769bf9ed5a3b8b7e6bf7e83a21875e5820e9177
rglover <me@ryanglover.net>
2024-07-22 16:00:39 -0500

    wip debugging child state

--- ui/src/component/render_methods/component.js
+++ ui/src/component/render_methods/component.js
@@ -17,9 +17,11 @@ const component = function component(Component = {}, props = {}) {
 	component_instance.parent = parent;
 
 	add_node_to_tree(component_instance);
+	parent.track_child(component_instance);
+
+	const new_component_on_parent = parent?.new_children[component_instance?.id];
 
 	const existing_component_on_parent = parent?.existing_children[component_instance?.id];
-	const new_component_on_parent = parent?.new_children[component_instance?.id];
 	const existing_instance_id_on_parent = existing_component_on_parent && existing_component_on_parent[(new_component_on_parent?.length - 1) || 0];
 	const existing_node_in_tree = get_node_from_tree(existing_instance_id_on_parent);
 
@@ -52,8 +54,6 @@ const component = function component(Component = {}, props = {}) {
 	component_instance.DOMNode = dom;
 	component_instance.virtual_dom = virtual_dom;
 	component_instance.children = new_children;
-
-	parent.track_child(component_instance);
 
 	return html;
 };
