09888c3a5c78582b54e69649331da39994e83c0b
rglover <me@ryanglover.net>
2024-07-22 22:28:22 -0500

    wip debugging child state

--- ui/src/tree/add_node_to_tree.js
+++ ui/src/tree/add_node_to_tree.js
@@ -1,3 +1,5 @@
+import debounce from '../lib/debounce.js';
+
 const add_node_to_tree = (node = {}, ssr_tree = null) => {
   const tree_on_window = typeof window !== 'undefined' ? window.joystick._internal.tree : [];
   (ssr_tree || tree_on_window).push(node);
