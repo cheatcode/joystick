66c65d47b5f5c4ad16ada336f6f9720fb664c434
rglover <me@ryanglover.net>
2024-07-25 01:19:50 -0500

    wip dynamic pages

---
+++ ui/src/component/dynamic_page/index.js
@@ -0,0 +1,9 @@
+const load_dynamic_page = (dynamic_page_options = {}) => {
+  console.log(dynamic_page_options);
+};
+
+const dynamic_page = {
+  load: load_dynamic_page,
+};
+
+export default dynamic_page;
