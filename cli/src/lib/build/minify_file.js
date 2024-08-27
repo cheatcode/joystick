aa582df9c2c295898877cd1692be051f413069b9
rglover <me@ryanglover.net>
2024-08-26 22:07:31 -0500

    wip push-v2

--- cli/src/lib/build/minify_file.js
+++ cli/src/lib/build/minify_file.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { minify } from "terser";
+import esbuild from "esbuild";
 import path_exists from "../path_exists.js";
 
 const { readFile, writeFile } = fs.promises;
@@ -8,7 +8,9 @@ const minify_file = async (path = "") => {
   if (await path_exists(path)) {
     const file = await readFile(path, "utf-8");
     console.log(path);
-    const minified = await minify(file).catch((error) => {
+    const minified = await esbuild.transform(file, {
+      minify: true,
+    }).catch((error) => {
       console.warn(error);
     });
 
