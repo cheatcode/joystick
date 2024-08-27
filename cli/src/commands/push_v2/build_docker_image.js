8051600472a4a0fb591c834015e498a4e4919040
rglover <me@ryanglover.net>
2024-08-27 01:54:43 -0500

    wip push-2

--- cli/src/commands/push_v2/build_docker_image.js
+++ cli/src/commands/push_v2/build_docker_image.js
@@ -12,7 +12,78 @@ const __dirname = path.dirname(__filename);
 
 const joystick_docker_path = path.join(os.homedir(), '.joystick', 'docker');
 
-// ... (previous functions remain the same)
+const get_docker_binary_path = () => {
+  return path.join(joystick_docker_path, os.platform() === 'win32' ? 'docker.exe' : 'docker');
+};
+
+const check_docker_installation = () => {
+  const docker_path = get_docker_binary_path();
+  try {
+    execSync(`"${docker_path}" --version`, { stdio: 'ignore' });
+    return true;
+  } catch (error) {
+    console.warn('Warning: Docker is not installed or the binary is not accessible.');
+    return false;
+  }
+};
+
+const download_file = (url, dest) => {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    https.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(resolve);
+      });
+    }).on('error', (error) => {
+      fs.unlink(dest, () => reject(error));
+    });
+  });
+};
+
+const get_docker_binary = async () => {
+  const platform = os.platform();
+  const arch = os.arch();
+  let url;
+  let archive_name;
+
+  if (platform === 'linux') {
+    url = `https://download.docker.com/linux/static/stable/${arch}/docker-24.0.7.tgz`;
+    archive_name = 'docker.tgz';
+  } else if (platform === 'darwin') {
+    url = `https://download.docker.com/mac/static/stable/${arch}/docker-24.0.7.tgz`;
+    archive_name = 'docker.tgz';
+  } else if (platform === 'win32') {
+    url = 'https://download.docker.com/win/static/stable/x86_64/docker-24.0.7.zip';
+    archive_name = 'docker.zip';
+  } else {
+    throw new Error('Unsupported operating system');
+  }
+
+  fs.mkdirSync(joystick_docker_path, { recursive: true });
+  const archive_path = path.join(joystick_docker_path, archive_name);
+  await download_file(url, archive_path);
+
+  if (platform === 'win32') {
+    const zip = new AdmZip(archive_path);
+    zip.extractAllTo(joystick_docker_path, true);
+  } else {
+    await tar.x({
+      file: archive_path,
+      cwd: joystick_docker_path,
+      strip: 1
+    });
+  }
+
+  fs.unlinkSync(archive_path);
+
+  // Make the docker binary executable on Unix-like systems
+  if (platform !== 'win32') {
+    fs.chmodSync(get_docker_binary_path(), '755');
+  }
+
+  process.loader.print(`Docker binaries downloaded and extracted to ${joystick_docker_path}`);
+};
 
 const build_docker_image = (
   image_name = '',
