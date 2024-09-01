97d025a2dc42d02cc283a3f1f34d34328eca3be6
rglover <me@ryanglover.net>
2024-08-31 19:17:12 -0500

    wip push-v2

--- cli/src/commands/push_v2/get_push_domain.js
+++ cli/src/commands/push_v2/get_push_domain.js
@@ -1,6 +1,6 @@
 const get_push_domain = (push_server = 'production') => {
 	return {
-		development: 'http://localhost:2603',
+		development: 'http://localhost:2600',
 		production: 'https://push.cheatcode.co',
 	}[push_server];
 };
