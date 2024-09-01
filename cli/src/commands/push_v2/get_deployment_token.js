4a4f6fef1aa0808581dfd22757df74a4eb68865a
rglover <me@ryanglover.net>
2024-08-31 19:15:44 -0500

    wip push-v2

--- cli/src/commands/push_v2/get_deployment_token.js
+++ cli/src/commands/push_v2/get_deployment_token.js
@@ -5,6 +5,8 @@ import path_exists from '../../lib/path_exists.js';
 const { readFile, mkdir, writeFile } = fs.promises;
 
 const get_deployment_token = async (deployment_token = '') => { 
+  const home_directory = os.homedir();
+
   if (deployment_token) {
 	  if (!(await path_exists(`${home_directory}/.push`))) {
 	    await mkdir(`${home_directory}/.push`);
@@ -15,12 +17,11 @@ const get_deployment_token = async (deployment_token = '') => {
 	  return deployment_token;
   }
 
-  const home_directory = os.homedir();
-  const session_token_path = `${home_directory}/.push/deployment_token`;
-  const session_token_exists = await path_exists(session_token_path);
+  const deployment_token_path = `${home_directory}/.push/deployment_token`;
+  const deployment_token_exists = await path_exists(deployment_token_path);
 
-  if (session_token_exists) {
-    return readFile(session_token_path, 'utf-8');
+  if (deployment_token_exists) {
+    return readFile(deployment_token_path, 'utf-8');
   }
 
   return null;
