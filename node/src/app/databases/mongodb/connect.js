0f983ef3c022fd2840f30e108dea7ca6500c994e
rglover <me@ryanglover.net>
2024-10-06 18:25:17 -0500

    wip debugging mongodb connection

--- node/src/app/databases/mongodb/connect.js
+++ node/src/app/databases/mongodb/connect.js
@@ -27,6 +27,8 @@ const connect_mongodb = async (database_settings = {}, database_port = 2610) => 
       connection_options.ca = fs.readFileSync(database_settings?.options?.ca);
     }
 
+    console.log({ connection_string, connection_options });
+
     const client = await MongoClient.connect(connection_string, connection_options);
     const db = client.db(parsed_uri.db);
 
