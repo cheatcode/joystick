2c61607299c6fe9252bea0af5c922b3756e2edd3
rglover <me@ryanglover.net>
2024-10-05 20:11:14 -0500

    migrating push_v2 to push

--- cli/src/commands/index.js
+++ cli/src/commands/index.js
@@ -3,7 +3,7 @@ import create from './create/definition.js';
 import help from './help/definition.js';
 import logout from './logout/definition.js';
 import push from './push/definition.js';
-import push_v2 from './push_v2/definition.js';
+// import push_v2 from './push_v2/definition.js';
 import start from './start/definition.js';
 import test from "./test/definition.js";
 import update from './update/definition.js';
