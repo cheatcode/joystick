1b67e0b7d9a53439ed4f1411263f60461d472cf7
rglover <me@ryanglover.net>
2024-12-08 22:42:28 -0600

    remove old HTML_ENTITY_MAP from constants

--- node/src/lib/constants.js
+++ node/src/lib/constants.js
@@ -1,11 +1,3 @@
-export const HTML_ENTITY_MAP = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;',
-};
-
 export const KILOBYTE = 1000;
 export const MEGABYTE = KILOBYTE * 1000;
 
