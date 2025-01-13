d58a6a930abae4b42735e90dec5fc5f8a605fba7
rglover <me@ryanglover.net>
2025-01-12 22:59:04 -0600

    wip debugging mod css tree shaking

--- node/src/lib/tree_shake_mod_css.js
+++ node/src/lib/tree_shake_mod_css.js
@@ -9,16 +9,15 @@ const purge_css_string = async (html = '', css = '') => {
     css: [{
       raw: css,
     }],
-    safelist: [
-      '[data-mod-theme="light"]',
-      '[data-mod-theme=\'light\']',
-      '[data-mod-theme=light]',
-      '[data-mod-theme="dark"]',
-      '[data-mod-theme=\'dark\']',
-      '[data-mod-theme=dark]',
-      '.mod-modal',
-      '.mod-dialog'
-    ]
+    safelist: {
+      standard: [
+        'mod-modal',
+        'mod-dialog'
+      ],
+      deep: [
+        /data-mod-theme/
+      ]
+    }
   });
   
   return purge_result[0] && purge_result[0].css || '';
