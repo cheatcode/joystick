2ffbe3308e86b700fcf488b0b265e319d132ab5f
rglover <me@ryanglover.net>
2025-01-15 16:26:51 -0600

    wip debugging mod tree shaking

--- node/src/lib/get_mod_css_from_map.js
+++ node/src/lib/get_mod_css_from_map.js
@@ -1,14 +1,15 @@
 const filter_icons_css = (icons_css = '', required_icons = []) => {
-  // If no icons required, return empty string
   if (required_icons.length === 0) {
     return ''
   }
+
+  const [base_styles, icon_styles] = icons_css.split('.split-point-for-tree-shaking {\n}')
   
   // Create regex pattern to match unwanted icon selectors
   const keep_icons = required_icons.map(name => `mod-icon-${name}`).join('|')
   const remove_pattern = new RegExp(`\\.mod-icon-(?!(${keep_icons})[^{]+){[^}]+}`, 'g')
   
-  return icons_css.replace(remove_pattern, '')
+  return base_styles + icon_styles.replace(remove_pattern, '')
 }
 
 const get_mod_css_from_map = (map = {}, keep_list = [], theme = 'light') => {
@@ -25,11 +26,10 @@ const get_mod_css_from_map = (map = {}, keep_list = [], theme = 'light') => {
 
     // Add filtered icons CSS if icons exist in map.global
     if (map.global.icons) {
-      css += map.global.icons;
-      // const required_icons = keep_list.filter(item => item.startsWith('icon-'));
-      // const filtered_icons_css = filter_icons_css(map.global.icons, required_icons);
-      // console.log(filtered_icons_css);
-      // css += filtered_icons_css;
+      // css += map.global.icons;
+      const required_icons = keep_list.filter(item => item.startsWith('icon-'));
+      const filtered_icons_css = filter_icons_css(map.global.icons, required_icons);
+      css += filtered_icons_css;
     }
   }
 
