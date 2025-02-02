59c5795b4b3cd26de306a332b5f21ee3586b8056
rglover <me@ryanglover.net>
2025-02-01 20:02:45 -0600

    wip debugging attribute setting in dom

--- ui/src/component/virtual_dom/render_virtual_dom_to_dom.js
+++ ui/src/component/virtual_dom/render_virtual_dom_to_dom.js
@@ -1,56 +1,6 @@
 import is_valid_attribute_name from "../dom/is_valid_attribute_name.js";
 import types from '../../lib/types.js';
-
-const special_attributes = {
-  value: ['input', 'textarea', 'select', 'option'],
-  checked: ['input'],
-  selected: ['option'],
-  disabled: ['input', 'textarea', 'select', 'button', 'option'],
-  srcdoc: ['iframe'],
-  muted: ['video', 'audio'],
-  volume: ['video', 'audio'],
-  currenttime: ['video', 'audio'],
-  playbackrate: ['video', 'audio'],
-  indeterminate: ['input'],
-  readonly: ['input', 'textarea'],
-  selectedindex: ['select']
-};
-
-const set_special_attribute = (element, key, value) => {
-  const tag_name = element.tagName.toLowerCase();
-  const special_config = special_attributes[key];
-  
-  if (!special_config) {
-    return false;
-  }
-
-  const applies_to_element = special_config.includes(tag_name);
-  
-  if (!applies_to_element) {
-    return false;
-  }
-
-  if (typeof value === 'boolean') {
-    if (value) {
-      element[key] = true;
-      element.setAttribute(key, '');
-    } else {
-      element[key] = false;
-      element.removeAttribute(key);
-    }
-    return true;
-  }
-
-  if (key === 'srcdoc') {
-    const temp_div = document.createElement('div');
-    temp_div.textContent = value;
-    element.srcdoc = temp_div.textContent;
-    return true;
-  }
-
-  element[key] = value;
-  return true;
-};
+import { set_special_attribute } from './special_attributes.js';
 
 const render_dom_node = (virtual_dom_node = {}, options = {}) => {
   const element_is_svg = virtual_dom_node?.tag_name?.toLowerCase() === 'svg' || options?.is_svg_child;
