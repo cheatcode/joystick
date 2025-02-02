9fa0859ef6f8a1f3b7141aa54fd37ec520e624e3
rglover <me@ryanglover.net>
2025-02-01 18:42:35 -0600

    wip debugging req access on api context

--- node/src/app/api/get_api_context.js
+++ node/src/app/api/get_api_context.js
@@ -2,6 +2,8 @@ const get_api_context = (req = {}, res = {}, api_context = null) => {
   // NOTE: api_context is the optional *global* context defined in the API
   // schema at /api/index.js in an app.
   return new Promise(async (resolve) => {
+    console.log({ req, res });
+    
     if (typeof api_context === "function") {
       const compiled_api_context = await api_context(req, res);
 
