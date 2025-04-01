77e274cba5fb845dacaa8e2be4bf4c4ae2ba54fc
rglover <me@ryanglover.net>
2025-04-01 12:04:14 -0500

    remove debug log

--- node/src/app/push/index.js
+++ node/src/app/push/index.js
@@ -31,7 +31,6 @@ const push = () => {
   const sync_metrics_job = cron.schedule(NODE_CRON_EVERY_THIRTY_SECONDS, async () => {
     try {
       const metrics = await snapshot_metrics();
-      console.log(`[${new Date().toISOString()}] [metrics] job started`, JSON.stringify(metrics));
       send_instance_data_to_push('metrics', metrics);
     } catch (exception) {
       console.warn('sync_metrics_job', exception);
