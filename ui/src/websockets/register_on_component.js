e7b741f56e35a7ce5769c5357add09da12469d50
rglover <me@ryanglover.net>
2025-03-31 19:37:14 -0500

    wip attempt to centralize websocket connections per component

--- ui/src/websockets/register_on_component.js
+++ ui/src/websockets/register_on_component.js
@@ -8,19 +8,27 @@ const register_on_component = (component_options = {}, component_instance = {}) 
   for (let i = 0; i < websocket_definitions?.length; i += 1) {
     const [websocket_name, websocket_definition] = websocket_definitions[i];
 
-    websocket_client({
-      component_instance,
-      test: component_instance?.test,
-      url: `${window?.process?.env.NODE_ENV === 'development' ? 'ws' : 'wss'}://${location.host}/api/_websockets/${websocket_name}`,
-      options: websocket_definition?.options || {},
-      query: websocket_definition?.query || {},
-      events: websocket_definition?.events || {},
-    }, (websocket_connection = {}) => {
-      component_instance.websockets = {
-        ...(component_instance.websockets || {}),
-        [websocket_name]: websocket_connection,
+    if (!joystick?._internal?.websockets?.[component_instance.id]?.[websocket_name]) {
+      joystick._internal.websockets = {
+        ...(joystick?._internal?.websockets || {}),
+        [component_instance.id]: {
+          ...(joystick?._internal?.websockets?.[component_instance.id] || {}),
+          [websocket_name]: websocket_client({
+            component_instance,
+            test: component_instance?.test,
+            url: `${window?.process?.env.NODE_ENV === 'development' ? 'ws' : 'wss'}://${location.host}/api/_websockets/${websocket_name}`,
+            options: websocket_definition?.options || {},
+            query: websocket_definition?.query || {},
+            events: websocket_definition?.events || {},
+          }, (websocket_connection = {}) => {
+            component_instance.websockets = {
+              ...(component_instance.websockets || {}),
+              [websocket_name]: websocket_connection,
+            }
+          }),
+        },
       }
-    });
+    }
   }
 };
 
