bb0de9e51aef7461f86499249d34b3f758085a97
rglover <me@ryanglover.net>
2025-05-01 16:43:53 -0500

    wip debugging sessions on push data requests

--- node/src/app/middleware/session.js
+++ node/src/app/middleware/session.js
@@ -1,7 +1,26 @@
 import sessions_query from "../databases/queries/sessions.js";
 import set_cookie from '../../lib/set_cookie.js';
+import load_settings from "../settings/load.js";
 
 const session_middleware = async (req, res, next) => {
+  const push_instance_token = req?.headers?.['x-push-instance-token'];
+
+  if (push_instance_token) {
+    const settings = load_settings();
+
+    if (settings?.private?.cheatcode?.is_push) {
+      // NOTE: Only run this if the app has signaled that it's Push. If it is, we can
+      // trust we have access to the database and can verify the inbound token.
+      const instance = await process.databases.mongodb.collection('instances').findOne({
+        token: { $eq: push_instance_token },
+      });
+
+      if (!!instance) {
+        return next();
+      }
+    }
+  }
+
   if (req?.url?.includes('/api/_push/health')) {
     // NOTE: We know this is a health check from Push which doesn't require a
     // session to be created.
--- node/src/app/push/index.js
+++ node/src/app/push/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import cron from 'node-cron';
-import { NODE_CRON_EVERY_MINUTE, NODE_CRON_EVERY_TEN_SECONDS, NODE_CRON_EVERY_THIRTY_SECONDS } from "../../lib/constants.js";
+import { NODE_CRON_EVERY_TEN_SECONDS, NODE_CRON_EVERY_THIRTY_SECONDS } from "../../lib/constants.js";
 import path_exists from "../../lib/path_exists.js";
 import send_instance_data_to_push from './send_instance_data_to_push.js';
 import snapshot_metrics from './snapshot_metrics.js';
@@ -37,18 +37,8 @@ const push = () => {
     }
   });
 
-  // const health_check_job = cron.schedule(NODE_CRON_EVERY_TEN_SECONDS, () => {
-  //   try {
-  //     send_instance_data_to_push('health-checks');
-  //   } catch (exception) {
-  //     console.warn('health_check_job', exception);
-  //   }
-  // });
-
   sync_logs_job.start();
   sync_metrics_job.start();
-  
-  // health_check_job.start();
 };
 
 export default push;
