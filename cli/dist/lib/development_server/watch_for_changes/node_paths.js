2f658ed536d4733b32828381d5f9c2c0d19bfe5e
rglover <me@ryanglover.net>
2025-06-17 12:59:57 -0500

    wip adding support for workers directory

--- cli/dist/lib/development_server/watch_for_changes/node_paths.js
+++ cli/dist/lib/development_server/watch_for_changes/node_paths.js
@@ -1,2 +1,2 @@
-import e from"./get_platform_safe_path.js";var o=[e("api/"),e("caches/"),e("cronJobs/"),e("cron_jobs/"),e("fixtures/"),e("indexes/"),e("lib/node"),e("queues/"),e("routes/"),e("tests/"),e("uploaders/"),e("websockets/"),"index.server.js"];export{o as default};
+import e from"./get_platform_safe_path.js";var o=[e("api/"),e("caches/"),e("cronJobs/"),e("cron_jobs/"),e("fixtures/"),e("indexes/"),e("lib/node"),e("queues/"),e("routes/"),e("tests/"),e("uploaders/"),e("websockets/"),e("workers/"),"index.server.js"];export{o as default};
 //# sourceMappingURL=node_paths.js.map
