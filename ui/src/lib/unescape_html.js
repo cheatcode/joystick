81bb15271c0a0184ea9b743364d52ef2bdb381ed
Ryan Glover <me@ryanglover.net>
2025-08-04 18:19:15 -0500

    wip debugging escaping

--- ui/src/lib/unescape_html.js
+++ ui/src/lib/unescape_html.js
@@ -3,7 +3,7 @@ const unescape_html = (escaped_string) => {
     return escaped_string;
   }
   
-  return escaped_string
+  const unescaped_string = escaped_string
     .replace(/&amp;/g, "&")
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
@@ -12,6 +12,10 @@ const unescape_html = (escaped_string) => {
     .replace(/&#x2F;/g, "/")
     .replace(/&#x60;/g, "`")
     .replace(/&#x3D;/g, "=");
+
+  console.log({ escaped_string, unescaped_string });
+  
+  return unescaped_string; 
 };
 
 export default unescape_html;
