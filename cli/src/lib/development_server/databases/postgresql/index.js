b88095e6d7bb8ce7faf840d364d24ed53db92d35
Ryan Glover <me@ryanglover.net>
2025-08-07 15:48:11 -0500

    update postgresql paths to match new binary setup

--- cli/src/lib/development_server/databases/postgresql/index.js
+++ cli/src/lib/development_server/databases/postgresql/index.js
@@ -2,12 +2,23 @@ import child_process from "child_process";
 import fs from "fs";
 import util from "util";
 import os from "os";
+import path from "path";
 import get_platform_safe_path from "../../../get_platform_safe_path.js";
 import get_process_id_from_port from "../../../get_process_id_from_port.js";
 import path_exists from "../../../path_exists.js";
 
 const exec = util.promisify(child_process.exec);
 const { rename } = fs.promises;
+
+const get_postgresql_env = (pg_dir) => {
+  const env = { ...process.env };
+  
+  if (process.platform === 'darwin') {
+    env.DYLD_LIBRARY_PATH = path.join(pg_dir, 'lib');
+  }
+  
+  return env;
+};
 
 const setup_data_directory = async (postgresql_port = 2610) => {
   const legacy_data_directory_exists = await path_exists(".joystick/data/postgresql");
@@ -63,21 +74,25 @@ const start_postgresql = async (port = 2610) => {
     const joystick_postgres_command = get_postgres_command();
     const joystick_createdb_command = get_createdb_command();
     
-    const joystick_pg_ctl_path = `${joystick_postgresql_bin_path}/${joystick_pg_ctl_command}`;
-    const joystick_initdb_path = `${joystick_postgresql_bin_path}/${joystick_initdb_command}`;
-    const joystick_postgres_path = `${joystick_postgresql_bin_path}/${joystick_postgres_command}`;
-    const joystick_createdb_path = `${joystick_postgresql_bin_path}/${joystick_createdb_command}`;
+    const joystick_pg_ctl_path = `${joystick_postgresql_bin_path}/bin/${joystick_pg_ctl_command}`;
+    const joystick_initdb_path = `${joystick_postgresql_bin_path}/bin/${joystick_initdb_command}`;
+    const joystick_postgres_path = `${joystick_postgresql_bin_path}/bin/${joystick_postgres_command}`;
+    const joystick_createdb_path = `${joystick_postgresql_bin_path}/bin/${joystick_createdb_command}`;
 
     const data_directory_exists = await setup_data_directory(port);
 
     if (!data_directory_exists) {
-      await exec(`${joystick_initdb_path} -D .joystick/data/postgresql_${port} --no-locale`);
+      await exec(`${joystick_initdb_path} -D .joystick/data/postgresql_${port} --no-locale`, {
+        env: get_postgresql_env(joystick_postgresql_bin_path)
+      });
     }
 
     const existing_process_id = parseInt(await get_process_id_from_port(postgresql_port), 10);
 
     if (existing_process_id) {
-      await exec(`${joystick_pg_ctl_path} kill KILL ${existing_process_id}`);
+      await exec(`${joystick_pg_ctl_path} kill KILL ${existing_process_id}`, {
+        env: get_postgresql_env(joystick_postgresql_bin_path)
+      });
     }
 
     const database_process = child_process.spawn(
@@ -87,6 +102,9 @@ const start_postgresql = async (port = 2610) => {
         '-D',
         get_platform_safe_path(`.joystick/data/postgresql_${port}`),
       ],
+      {
+        env: get_postgresql_env(joystick_postgresql_bin_path)
+      }
     );
 
     return new Promise((resolve) => {
@@ -97,7 +115,9 @@ const start_postgresql = async (port = 2610) => {
           const process_id = (await get_process_id_from_port(postgresql_port))?.replace('\n', '');
           const createdb_command = `${joystick_createdb_path} -h 127.0.0.1 -p ${postgresql_port} app`;
 
-          exec(createdb_command).then(() => {
+          exec(createdb_command, {
+            env: get_postgresql_env(joystick_postgresql_bin_path)
+          }).then(() => {
             resolve(parseInt(process_id, 10));
           }).catch(({ stderr: error }) => {
             if (error && error.includes('database "app" already exists')) {
