5fb65322e0168f29452db830af630bb1fdf541fc
Ryan Glover <me@ryanglover.net>
2025-08-10 18:34:10 -0500

    wip finding minimum es version for browser builds

--- cli/src/lib/build/build_files.js
+++ cli/src/lib/build/build_files.js
@@ -82,7 +82,7 @@ const handle_build_for_node = (node_paths = [], options = {}) => {
 const handle_build_for_browser = (browser_paths = [], options = {}) => {
   return esbuild.build({
     allowOverwrite: true,
-    target: "es2022",
+    target: "es2021",
     platform: "browser",
     format: "esm",
     bundle: true,
