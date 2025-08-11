bb0503479405e8317307381eb9ed12702e97c139
Ryan Glover <me@ryanglover.net>
2025-08-10 22:59:37 -0500

    wip testing html render method

---
+++ ui/src/component/render_methods/html.js
@@ -0,0 +1,7 @@
+const html = (string = '') => {
+  // NOTE: Intentionally simple. Gives developers a way to use lit-html in their
+  // IDE without having to do anything special.
+  return string;
+};
+
+export default html;
--- ui/src/component/render_methods/index.js
+++ ui/src/component/render_methods/index.js
@@ -1,6 +1,7 @@
 import component from './component.js';
 import component_ssr from './component_ssr.js';
 import each from './each.js';
+import html from './html.js';
 import i18n from './i18n.js';
 import raw from './raw.js';
 import when from './when.js';
@@ -11,6 +12,8 @@ const render_methods = {
 	component_ssr,
 	e: each,
 	each,
+	h: html,
+	html,
 	i: i18n,
 	i18n,
 	r: raw,
