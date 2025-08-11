1d1343e34dce792e09189d3160cad0545199a3e1
Ryan Glover <me@ryanglover.net>
2025-08-11 16:29:15 -0500

    wip debugging databases in tests

--- cli/dist/lib/development_server/ava_config.js
+++ cli/dist/lib/development_server/ava_config.js
@@ -1,2 +1,2 @@
-const e={files:["tests/**/*.test.js"],ignoredByWatcher:[".joystick","./uploads"],concurrency:1,cache:!1};var c=e;export{c as default};
+const e={files:["tests/**/*.test.js"],watchMode:{ignoreChanges:[".joystick","./uploads"]},concurrency:1,cache:!1};var s=e;export{s as default};
 //# sourceMappingURL=ava_config.js.map
--- cli/src/lib/development_server/ava_config.js
+++ cli/src/lib/development_server/ava_config.js
@@ -2,7 +2,9 @@ const ava_config = {
   files: [
     `tests/**/*.test.js`,
   ],
-  ignoredByWatcher: ['.joystick', './uploads'],
+  watchMode: {
+    ignoreChanges: ['.joystick', './uploads'],
+  },
   // NOTE: Intentionally limit to 1 test at a time to avoid concurrency creating
   // race conditions in tests that have users or other global data that needs
   // to be cleaned up. The extra time needed is worth it to guarantee that tests
