e1ba570abbe24d8a67d6288fa45133dc3c0d6ef4
Ryan Glover <me@ryanglover.net>
2025-08-10 23:57:26 -0500

    wip debugging html render method

--- ui/src/component/render_methods/html.js
+++ ui/src/component/render_methods/html.js
@@ -1,7 +1,16 @@
-const html = (string = '') => {
+const html = (strings, ...values) => {
   // NOTE: Intentionally simple. Gives developers a way to use lit-html in their
   // IDE without having to do anything special.
-  return string;
+  let result = '';
+  
+  for (let i = 0; i < strings.length; i++) {
+    result += strings[i];
+    if (i < values.length) {
+      result += values[i];
+    }
+  }
+  
+  return result;
 };
 
 export default html;
