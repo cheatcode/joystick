baffd318f0c18f00f41d28aade7ae7dd466d3d6b
Ryan Glover <me@ryanglover.net>
2025-08-11 18:02:48 -0500

    wip debugging get_function_calls

--- test/src/helpers/utils/get_function_calls.js
+++ test/src/helpers/utils/get_function_calls.js
@@ -3,7 +3,11 @@ import fetch from 'node-fetch';
 const get_function_calls = async (path = '') => {
   const server = await fetch(
     `http://localhost:${process.env.PORT}/api/_test/process`
-  ).then((response) => response.json());
+  ).then((response) => {
+    const data = response.json();
+    console.log({ response, data });
+    return data;
+  });
   
   let calls = {
     ...(server?.test?.function_calls || {}),
