64d2e05d69d68f8f04875134157fe493d75963a8
Ryan Glover <me@ryanglover.net>
2025-08-19 18:56:57 -0500

    remove gitignore respect in builds to avoid blocking files that should be in deployment but not in repo

--- cli/dist/lib/build/get_tar_ignore_list.js
+++ cli/dist/lib/build/get_tar_ignore_list.js
@@ -1,3 +1,2 @@
-import r from"fs";import o from"./master_ignore_list.js";const s=(e=[])=>`{${[...((r.existsSync(".gitignore")?r.readFileSync(".gitignore","utf-8"):"")?.split(`
-`)?.filter(t=>!t?.includes("#")&&t?.trim()!=="")||[])?.map(t=>`^${t}`),...(e||[])?.map(t=>`^${t}`),...(o||[])?.map(t=>`^${t}`),"*.tar","*.tar.gz","*.tar.xz"].filter((t,i,n)=>n.indexOf(t)===i)?.map(t=>`"${t}"`).join(",")}}`;var c=s;export{c as default};
+import"fs";import o from"./master_ignore_list.js";const n=(r=[])=>`{${[...(r||[])?.map(t=>`^${t}`),...(o||[])?.map(t=>`^${t}`),"*.tar","*.tar.gz","*.tar.xz"].filter((t,e,i)=>i.indexOf(t)===e)?.map(t=>`"${t}"`).join(",")}}`;var m=n;export{m as default};
 //# sourceMappingURL=get_tar_ignore_list.js.map
--- cli/src/lib/build/get_tar_ignore_list.js
+++ cli/src/lib/build/get_tar_ignore_list.js
@@ -2,22 +2,11 @@ import fs from "fs";
 import master_ignore_list from "./master_ignore_list.js";
 
 const get_tar_ignore_list = (excluded_paths = []) => {
-  const git_ignore = fs.existsSync(".gitignore")
-    ? fs.readFileSync(".gitignore", "utf-8")
-    : "";
-    
-  const git_ignore_files = git_ignore?.split("\n")?.filter((file) => {
-    return !file?.includes("#") && file?.trim() !== "";
-  });
-
   // NOTE: The ^ character is an anchor to tell tar to only match the pattern
   // to the root path. Without this, it will match all nested paths that resemble
   // the pattern you give it. For example, ./versions will match the api/versions
   // directory too.
   const files_to_ignore = [
-    ...(git_ignore_files || [])?.map((ignore) => {
-      return `^${ignore}`;
-    }),
     ...(excluded_paths || [])?.map((ignore) => {
       return `^${ignore}`;
     }),
